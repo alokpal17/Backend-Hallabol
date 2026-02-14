bd3bfc07e77e8133d4ad6d8aebd20cfb71fbcdfc
Alok <alokp5491@gmail.com>
2026-02-14 11:30:36 +0530

    fix: Correct export statement for Subscription model

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -15,4 +15,4 @@ const subscriptionSchema = new Schema({
 
 
 
-export const Subscription - mongoose.model("Subscription", subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
