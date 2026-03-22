c85c4391341e92a8a3f7235b9b2c383b36538698
Alok <alokp5491@gmail.com>
2026-03-22 07:33:49 +0530

    feat: Refactor routes and controllers for consistency; add tweet routes

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
+import {Comment} from "../models/comment.models.js"
 import {Video} from "../models/video.models.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
